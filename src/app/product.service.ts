f82408d43528194f28697476ece890d80bfef2fd
ogbevireo <ogbevire@gmail.com>
2020-09-27 10:28:49 +0100

    import album interface and add a return type to getAlbum Method

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,7 +1,9 @@
 import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
+import { Album } from './album';
 
 import 'rxjs/add/operator/map';
+import 'rxjs/observable';
 
 @Injectable()
 export class ProductService {
@@ -11,7 +13,7 @@ export class ProductService {
 
   constructor(private _http: Http) { }
 
-  getAlbum(id: number) {
+  getAlbum(id: number) observable<Album> {
     return this._http.get(this._albumUrl).map((response) => response.json());
 
 
