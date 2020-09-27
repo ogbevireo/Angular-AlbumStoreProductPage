8c51a8b6d655be5a01fc99b17489eb5430e29dbb
ogbevireo <ogbevire@gmail.com>
2020-09-27 11:44:22 +0100

    update track listing with the ngFor command

--- src/app/product-tracklisting/product-tracklisting.component.ts
+++ src/app/product-tracklisting/product-tracklisting.component.ts
@@ -14,7 +14,7 @@ export class ProductTracklistingComponent implements OnInit {
   constructor(private _productService: ProductService) { }
 
   ngOnInit() {
-    this._ProductService.getAlbum(1).subscribe(response => this.albumInfo = response);
+    this._productService.getAlbum(1).subscribe(response => this.albumInfo = response);
   }
 
 }
