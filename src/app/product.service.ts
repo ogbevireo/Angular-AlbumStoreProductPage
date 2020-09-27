9d88f276a1d21e4d17a34d61ba90020f73102be0
ogbevireo <ogbevire@gmail.com>
2020-09-27 12:37:06 +0100

    grab products list dynamically

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
 import { Album } from './album';
+import { Product } from './product';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
 
@@ -9,6 +10,7 @@ import 'rxjs/add/operator/map';
 export class ProductService {
 
   private _albumUrl = '../assets/album.json';
+  private _productsUrl = '../assets/products.json';
   
 
   constructor(private _http: Http) { }
@@ -17,6 +19,9 @@ export class ProductService {
     return this._http.get(this._albumUrl).map(response => <Album>response.json());
 
 
+  }
+  getProducts(): Observable<Product[]>{
+    retrurn this._http.get(this._productsUrl).map(response => <Product[]>response.json());
   }
 
 
